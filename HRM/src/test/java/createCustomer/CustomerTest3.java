b8b995b157111a9dc600de82a15a4a33e6363c1c
MD IMRAN ANSARI <MD IMRAN ANSARI@Dell-Inspiron-5501>
2023-01-23 12:50:07 +0530

    s

---
+++ HRM/src/test/java/createCustomer/CustomerTest3.java
@@ -0,0 +1,14 @@
+package createCustomer;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerTest3 {
+	@Test 
+	public void a() {
+		Reporter.log("a", true);
+		System.out.println("abc");
+		
+	}
+}
